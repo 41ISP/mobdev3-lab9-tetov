cb02f52a09a4dce39be85286917588ad139b51b6
Andrey <andrey.28.06@mail.ru>
2025-10-28 03:10:54 +0000

    swa

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: 'mobdev-lab9-tetov'
+   base: 'mobdev3-lab9-tetov'
 })
